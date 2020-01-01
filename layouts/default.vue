591b1e48aa6c9d245a469d6a71091480f7864dc0
Denis Mylnikov <denis@Deniss-MacBook-Pro.local>
2020-01-01 17:38:57 +0100

    added sass

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,15 +35,7 @@ export default {}
   margin: 0;
   box-sizing: border-box;
 }
-html, body {
-  background-color: #fff;
-  color: #2e2f30;
-  letter-spacing: 0.5px;
-  font-size: 18px;
-  font-family: "Source Sans Pro", Arial, sans-serif;
-  height: 100vh;
-  margin: 0;
-}
+
 .container {
   width: 75%;
   margin: 0 auto;
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Quicksand&display=swap'}
     ]
   },
   /*
@@ -22,8 +23,7 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['@/assets/main.scss'],
   /*
   ** Plugins to load before mounting the App
   */
