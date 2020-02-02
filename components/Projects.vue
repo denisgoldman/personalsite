c89e2f037738b7b9554d062661c52be9dc98b53e
Denis Goldman <denis@Deniss-MacBook-Pro.local>
2020-02-02 20:03:24 +0100

    added projects header

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,9 +1,15 @@
 <template>
+<div class="projects">
+    <div class="projects__header">
+        <h2>{{$t('projects.title')}}</h2>
+        <h4 class="projects__subtitle">{{$t('projects.subtitle')}}</h4>
+    </div>
   <div class="grid">
       <ProjectCard class="span-3" :title="($t('projects.rolex.title'))" :description="($t('projects.rolex.subtitle'))" image="rolexpackagingsolid.jpg" />
       <!-- <ProjectCard class="span-1" :title="('Amazing project')" :description="('Cool stuff')" image="preview.jpeg" /> -->
       <!-- <ProjectCard class="span-2" :title="('Amazing project')" :description="('Cool stuff')" image="preview.jpeg" /> -->
       <!-- <ProjectCard class="span-3" :title="('Amazing project')" :description="('Cool stuff')" image="preview.jpeg" /> -->
+  </div>
   </div>
 </template>
 
@@ -18,6 +24,26 @@ export default {
 </script>
 
 <style lang="scss">
+
+    .projects {
+        margin: 0 -5px;
+        &__header {
+            padding: 20px 20px 40px 20px;
+            margin-bottom: -5px;
+            text-align: center;
+            background: $areaColor_light;
+            @media(prefers-color-scheme: dark) {
+                background: $areaColor_dark;
+            }
+        }
+
+        &__subtitle {
+             @media (prefers-color-scheme: dark) {
+          color: $secondaryTextColor;
+      }
+        }
+    }
+
     .grid {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
--- components/Skills.vue
+++ components/Skills.vue
@@ -42,7 +42,7 @@ export default {
 <style lang="scss">
 .skills {
   margin-top: 10px;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
   background: $areaColor_light;
   @media (prefers-color-scheme: dark) {
     background: $areaColor_dark;
