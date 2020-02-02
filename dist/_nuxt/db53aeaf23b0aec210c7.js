fec0fa42d6b8ece3c1fe37907b9102f09da12418
Denis Goldman <denis@Deniss-MacBook-Pro.local>
2020-02-02 19:36:08 +0100

    build

---
+++ dist/_nuxt/db53aeaf23b0aec210c7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{193:function(n,l,o){"use strict";o.r(l);var e=o(191).default,t=o(6),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
